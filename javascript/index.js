39328b7952e93989e259946fd6bc729c3658ca0d
Hyper-Cube <michaeltylerwhite@gmail.com>
2017-02-20 23:16:27 -0500

    Colorize cube / Add contents, and basic functionality

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,33 @@
 function init() {
+  initButtons();
   hover();
 }
+
+function initButtons() {
+  $('h3').click(function() {
+    switch(this.classList[0]) {
+      case "to-front":
+        $("#cube").toggleClass("show-front");
+        break;
+      case "to-back":
+        $("#cube").toggleClass("show-back");
+        break;
+      case "to-left":
+        $("#cube").toggleClass("show-left");
+        break;
+      case "to-right":
+        $("#cube").toggleClass("show-right");
+        break;
+      case "to-top":
+        $("#cube").toggleClass("show-top");
+        break;
+      case "to-bottom":
+        $("#cube").toggleClass("show-bottom");
+        break;
+    }
+  });
+}
+
 function hover() {
   document.onmousemove = function(event) {
     cube = document.getElementById("cube").children;
@@ -8,4 +35,6 @@ function hover() {
     }
   }
 }
+
+
 init();
