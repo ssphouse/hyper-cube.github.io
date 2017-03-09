ea8b8ff609cca385814cfc4bfd6f47338b741af0
Hyper-Cube <michaeltylerwhite@gmail.com>
2017-03-09 18:57:37 -0500

    Animate particles

--- javascript/index.js
+++ javascript/index.js
@@ -1,31 +1,58 @@
 function init() {
   initParticles();
+  changeParticleColor();
   changeSides();
   changeSubtitle();
+  moveParticle();
 }
 
 // Create an amount of particles on screen
+// Positions are random. opacity is random to give sense of depth
 function initParticles() {
   var height = $(window).height();
   var width = $(window).width();
 
-  var particleCount = 500;
+  var particleCount = 300;
   for(var i = 0; i < particleCount; i++) {
     $('.particle-container').append(
       '<span class="particles" style="top: ' + rand(1, height) + 'px; ' +
       'left: ' + rand(1, width) +
-      'px; background-color: rgba(23, 27, 39,'+ rand(0.4, 0.8, true) + ')"</span>'
+      'px; "</span>'
     );
   }
 }
 
-// Get a random number from a set of values and return it as a string
+function moveParticle() {
+  setInterval(function() {
+    $('.particles').each(function() {
+      var x = parseInt($(this).css('top').slice(0, -2)) + parseInt(rand(1, 100));
+      var y = parseInt($(this).css('left').slice(0, -2)) + parseInt(rand(-30, 100));
+      $(this).stop(true, false);
+      $(this).css('top', x.toString() + 'px');
+      $(this).css('left', y.toString() + 'px');
+    });
+  }, 4000);
+
+}
+
 function rand(start, end, decimal=false) {
   var result;
   decimal ?
     result = (Math.random() * end).toFixed(2) :
     result = Math.floor((Math.random() * end) + start);
   return result.toString();
+}
+
+function changeParticleColor() {
+  setInterval(function() {
+    $('.particles').each(function() {
+      var red = rand(45, 60);
+      var green = rand(20, 30);
+      var blue = rand(55, 35);
+      var opacity = rand(0.4, 0.85, true);
+      $(this).css('background-color', `rgba(${red}, ${green}, ${blue}, ${opacity})`);
+    });
+  }, 6000);
 }
 
 // Control which side we want facing
