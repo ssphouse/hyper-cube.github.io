8560892ad60b80434b088597f7bd8b485f1c5c22
Hyper-Cube <michaeltylerwhite@gmail.com>
2017-03-14 21:42:29 -0400

    Make cube responsive

--- javascript/index.js
+++ javascript/index.js
@@ -1,58 +1,20 @@
 function init() {
-  initParticles();
+  initCube();
   changeParticleColor();
   changeSides();
   changeSubtitle();
   moveParticle();
 }
 
-// Create an amount of particles on screen
-// Positions are random. opacity is random to give sense of depth
-function initParticles() {
-  var height = $(window).height();
-  var width = $(window).width();
-
-  var particleCount = 300;
-  for(var i = 0; i < particleCount; i++) {
-    $('.particle-container').append(
-      '<span class="particles" style="top: ' + rand(1, height) + 'px; ' +
-      'left: ' + rand(1, width) +
-      'px; "</span>'
-    );
-  }
-}
-
-function moveParticle() {
-  setInterval(function() {
-    $('.particles').each(function() {
-      var x = parseInt($(this).css('top').slice(0, -2)) + parseInt(rand(1, 100));
-      var y = parseInt($(this).css('left').slice(0, -2)) + parseInt(rand(-30, 100));
-      $(this).stop(true, false);
-      $(this).css('top', x.toString() + 'px');
-      $(this).css('left', y.toString() + 'px');
+function initCube() {
+  // Make the Container responsive
+  $(window).resize(function() {
+    var size = ($(document).width() * 0.35).toString();
+    $('.container').css({
+      "width": size + 'px',
+      "height": size + 'px'
     });
-  }, 4000);
-
-}
-
-function rand(start, end, decimal=false) {
-  var result;
-  decimal ?
-    result = (Math.random() * end).toFixed(2) :
-    result = Math.floor((Math.random() * end) + start);
-  return result.toString();
-}
-
-function changeParticleColor() {
-  setInterval(function() {
-    $('.particles').each(function() {
-      var red = rand(45, 60);
-      var green = rand(20, 30);
-      var blue = rand(55, 35);
-      var opacity = rand(0.4, 0.85, true);
-      $(this).css('background-color', `rgba(${red}, ${green}, ${blue}, ${opacity})`);
-    });
-  }, 6000);
+  });
 }
 
 // Control which side we want facing
@@ -70,6 +32,12 @@ function changeSides() {
         break;
       case 'to-right':
         $('#cube').attr('class', 'show-right');
+        $('#cube').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          function(event) {
+            if (event.target.id == "projects") {
+              console.log("hi");
+            }
+        });
         break;
       case 'to-top':
         $('#cube').attr('class', 'show-top');
@@ -99,5 +67,58 @@ function changeSubtitle() {
     subtitleFade('Front-End Developer');
   });
 }
+
+/*
+ * Create an amount of particles on screen
+ * Positions are random. opacity is random to give sense of depth
+ * Todo: Move particle related stuff to another file
+ */
+function initParticles() {
+  var height = $(window).height();
+  var width = $(window).width();
+
+  var particleCount = 300;
+  for(var i = 0; i < particleCount; i++) {
+    $('.particle-container').append(
+      '<span class="particles" style="top: ' + rand(1, height) + 'px; ' +
+      'left: ' + rand(1, width) +
+      'px; "</span>'
+    );
+  }
+}
+
+function moveParticle() {
+  setInterval(function() {
+    $('.particles').each(function() {
+      var x = parseInt($(this).css('top').slice(0, -2)) + parseInt(rand(1, 100));
+      var y = parseInt($(this).css('left').slice(0, -2)) + parseInt(rand(-30, 100));
+      $(this).stop(true, false);
+      $(this).css('top', x.toString() + 'px');
+      $(this).css('left', y.toString() + 'px');
+    });
+  }, 4000);
+
+}
+
+function changeParticleColor() {
+  setInterval(function() {
+    $('.particles').each(function() {
+      var red = rand(45, 60);
+      var green = rand(20, 30);
+      var blue = rand(55, 35);
+      var opacity = rand(0.4, 0.85, true);
+      $(this).css('background-color', `rgba(${red}, ${green}, ${blue}, ${opacity})`);
+    });
+  }, 6000);
+}
+
+function rand(start, end, decimal=false) {
+  var result;
+  decimal ?
+    result = (Math.random() * end).toFixed(2) :
+    result = Math.floor((Math.random() * end) + start);
+  return result.toString();
+}
+
 
 init();
